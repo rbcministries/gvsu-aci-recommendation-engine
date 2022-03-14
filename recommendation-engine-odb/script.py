dcaa5ba77df7a6d312c4a2debf88d8d74af67093
Thomas Lobbestael <tlobbestael@gmail.com>
2022-03-14 14:40:33 -0400

    Sagemaker progress and sample data upload

--- recommendation-engine-odb/script.py
+++ recommendation-engine-odb/script.py
@@ -4,10 +4,12 @@ import os
 import numpy as np
 import json
 
-# Lots of this is tentative but this is the actual script that sets the model
 
 def _load_training_data(base_dir):
-    df_train = np.load(os.path.join(base_dir, 'train.npy'))
+    df_train = np.load(os.path.join(base_dir, 'train.npy'), allow_pickle=True)
+    cols = df_train.select_dtypes(include=['string'])
+    for col in cols.columns.values:
+        df_train[col] = df_train[col].fillna('')
     user_train, item_train, y_train = np.split(np.transpose(df_train).flatten(), 3)
     return user_train, item_train, y_train
 
